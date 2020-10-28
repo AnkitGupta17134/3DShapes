35aa3283e7b148c1affc68db72c7a8e8a36c74cb
regex <32675731+AnkitGupta17134@users.noreply.github.com>
2020-10-28 09:57:45 +0530

    Volume function implemented

--- Cuboid.java
+++ Cuboid.java
@@ -1,16 +1,33 @@
+import java.util.*;
 class Cuboid
 {
   int l;
   int b;
   int h;
-  
+  int x0;
+  int y0;
   public Cuboid(int l, int b, int h)
   {
-    
+    this.l = l;
+    this.b = b;
+    this.h = h;
+    this.x0 = 0;
+    this.y0 = 0;
+  }
+  public static int surfaceArea()
+  {
+    return 2*(this.l*this.b + this.l*this.h + this.b*this.h);
+  }
+  public static int volume()
+  {
+    return this.l*this.b*this.h;
   }
   public static void main(Strings[] args)
   {
-    
+    Scanner jk = new Scanner(System.in);
+    int l = jk.nextInt();
+    int b = jk.nextInt();
+    int h = jk.nextInt();
   }
   
 }
