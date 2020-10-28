fca41beafa241ee699856847d80dbc99e36e0b84
regex <32675731+AnkitGupta17134@users.noreply.github.com>
2020-10-28 09:54:04 +0530

    Input and Surface Area functions

--- Cuboid.java
+++ Cuboid.java
@@ -6,11 +6,20 @@ class Cuboid
   
   public Cuboid(int l, int b, int h)
   {
-    
+    this.l = l;
+    this.b = b;
+    this.h = h;
+  }
+  public static int surfaceArea()
+  {
+    return 2*(this.l*this.b + this.l*this.h + this.b*this.h);
   }
   public static void main(Strings[] args)
   {
-    
+    Scanner jk = new Scanner(System.in);
+    int l = jk.nextInt();
+    int b = jk.nextInt();
+    int h = jk.nextInt();
   }
   
 }
