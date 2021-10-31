c337479d705bc725e50a540744fd8df4e1bd4322
Vaibhav Jayant <43247883+d4rkvaibhav@users.noreply.github.com>
2021-10-31 23:19:35 +0530

    Update Hemisphere.java

--- Hemisphere.java
+++ Hemisphere.java
@@ -3,20 +3,26 @@ import java.util.*;
 class Hemisphere
 {
  int r;
+ 
+ //Constructor
  public Hemisphere(int r)
  {
   this.r = r;
  }
+ //Funtion for Surface area
  public static int sA()
  {
   return 2*Math.pi*r*r;
  }
+ 
  public static int tsA()
  {
   return 3*Math.pi*r*r;
  }
+ 
  public static void main(Strings[] args)
  {
+//   Taking Input
   Scanner jk = new Scanner(System.in);
   System.out.print("Enter Radius : ");
   int r = jk.nextInt();
