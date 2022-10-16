7d0a80ba054f226054ef03213fdcbb3eb42f26ed
remainisyours <44507677+remainisyours@users.noreply.github.com>
2022-10-16 23:11:24 +0530

    adding new attributes with getters setters
    Default is white

--- Cuboid.java
+++ Cuboid.java
@@ -4,11 +4,13 @@ public class Cuboid
   int l;
   int b;
   int h;
+  String color;
   public Cuboid(int l, int b, int h)
   {
     this.l = l;
     this.b = b;
     this.h = h;
+    this.color = "White";
   }
   public int surfaceArea()
   {
@@ -26,6 +28,14 @@ public class Cuboid
   {
     return this.l*this.b*this.h;
   }
+  public String getColor()
+  {
+    return this.color;
+  }
+  public void setColor(String color)
+  {
+    this.color = color;
+  }
   public static void main(Strings[] args)
   {
     Scanner jk = new Scanner(System.in);
@@ -37,6 +47,9 @@ public class Cuboid
     System.out.println();
     System.out.print("Input Height of Cuboid ");
     int h = jk.nextInt();
+    System.out.println();
+    System.out.println("Enter color of cuboid");
+    String colr = jk.nextLine();
     System.out.println();
     // Printing Surface Area and Volume
     Cuboid cuba = new Cuboid(2,3,4);
